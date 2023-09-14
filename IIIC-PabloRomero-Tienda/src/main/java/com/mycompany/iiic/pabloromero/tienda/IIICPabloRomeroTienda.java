e927e0e9a34dc5a0880490a92afac83a255d7d78
PromeroQ1207 <promero90942@ufide.ac.cr>
2023-09-13 20:46:40 -0600

    Semana 1 prueba
    1 commit de prueba

---
+++ IIIC-PabloRomero-Tienda/src/main/java/com/mycompany/iiic/pabloromero/tienda/IIICPabloRomeroTienda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.iiic.pabloromero.tienda;
+
+/**
+ *
+ * @author pablo
+ */
+public class IIICPabloRomeroTienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
