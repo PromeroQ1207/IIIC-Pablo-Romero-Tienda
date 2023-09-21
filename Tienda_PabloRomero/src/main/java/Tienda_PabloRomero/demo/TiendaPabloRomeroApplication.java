1edd13ff7bde0e4c9c85cded438dc60a98a0a126
PromeroQ1207 <promero90942@ufide.ac.cr>
2023-09-20 20:54:41 -0600

    cambio repositorio
    Semana 2

---
+++ Tienda_PabloRomero/src/main/java/Tienda_PabloRomero/demo/TiendaPabloRomeroApplication.java
@@ -0,0 +1,13 @@
+package Tienda_PabloRomero.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaPabloRomeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaPabloRomeroApplication.class, args);
+	}
+
+}
---
+++ Tienda_PabloRomero/src/test/java/Tienda_PabloRomero/demo/TiendaPabloRomeroApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_PabloRomero.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaPabloRomeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
